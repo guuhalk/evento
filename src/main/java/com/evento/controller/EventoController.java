8385701f0d8a958754fc447db850344bfda01f6d
Gustavo <gustavodaniel.santos@outlook.com>
2019-07-05 13:03:30 -0300

    primeiro commit

---
+++ src/main/java/com/evento/controller/EventoController.java
@@ -0,0 +1,17 @@
+package com.evento.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class EventoController {
+
+	@RequestMapping("/cadastrarEvento")
+	public String form() {
+		return "evento/formEvento";
+
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/evento/repository/EventoRepository.java
@@ -0,0 +1,9 @@
+package com.evento.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.evento.model.Evento;
+
+public interface EventoRepository  extends CrudRepository<Evento, Integer>{
+
+}
